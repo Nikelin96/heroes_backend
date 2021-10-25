4ace1a111fe2093e6fb2889066607effbeadb09c
Nikelin96 <n.revutskyi@gmail.com>
2021-10-25 20:16:32 +0300

    Testing postgresRepository
    Added more test cases for repository

--- src/node/postgresRepository.test.js
+++ src/node/postgresRepository.test.js
@@ -12,18 +12,71 @@ test('constructor returns instance', () => {
     expect(sut.client).toBe(clientMock);
 });
 
-test('getHeroesAsync calls client once', () => {
+test('getHeroesAsync returns heroes', async () => {
     // arrange
     const clientMock = {
-        query: jest.fn().mockImplementation(() => {
+        query: jest.fn().mockImplementation(async () => {
+            return { rows: [{ Id: 1, Name: "Name1" }, { Id: 2, Name: "Name2" }, { Id: 3, Name: "Name3" }] };
+        })
+    }
+    const expected = [{ id: 1, name: "Name1" }, { id: 2, name: "Name2" }, { id: 3, name: "Name3" }]
+
+    const sut = new PostgresRepository(clientMock);
+
+    // act
+    const actual = await sut.getHeroesAsync();
+
+    // assert
+    expect(actual).toStrictEqual(expected);
+    expect(sut.client.query).toHaveBeenCalledTimes(1);
+});
+
+test('deleteHeroAsync calls client once', async () => {
+    // arrange
+    const clientMock = {
+        query: jest.fn().mockImplementation(async () => {
             return { rows: [] };
         })
     }
 
-    let sut = new PostgresRepository(clientMock);
+    const sut = new PostgresRepository(clientMock);
 
     // act
-    sut.getHeroesAsync();
+    await sut.deleteHeroAsync(1);
+
+    // assert
+    expect(sut.client.query).toHaveBeenCalledTimes(1);
+});
+
+test('createHeroAsync calls client once', async () => {
+    // arrange
+    const clientMock = {
+        query: jest.fn().mockImplementation(async () => {
+            return { rows: [] };
+        })
+    }
+
+    const sut = new PostgresRepository(clientMock);
+
+    // act
+    await sut.createHeroAsync(1);
+
+    // assert
+    expect(sut.client.query).toHaveBeenCalledTimes(1);
+});
+
+test('updateHeroAsync calls client once', async () => {
+    // arrange
+    const clientMock = {
+        query: jest.fn().mockImplementation(async () => {
+            return { rows: [] };
+        })
+    }
+
+    const sut = new PostgresRepository(clientMock);
+
+    // act
+    await sut.updateHeroAsync(1);
 
     // assert
     expect(sut.client.query).toHaveBeenCalledTimes(1);
