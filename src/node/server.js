d7517ff1bb363ff2e3e1e69dc0a44dd3769177b3
mrevutskyi <n.revutskyi@gmail.com>
2021-11-16 12:38:48 +0200

    Created docker file
    Created docker file
    added minor fix for app

--- src/node/server.js
+++ src/node/server.js
@@ -1,8 +1,10 @@
-const app = require('./app.js');
+const { getApp } = require('./app');
 
 // setup listening
 const hostname = process.env.serverHost ?? 'localhost';
 const port = process.env.serverPort ?? 3000;
+
+const app = getApp();
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
